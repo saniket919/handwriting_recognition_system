2ddfc992548d0c881d490eb91f21c8d3b22acb65
Durgansh Dixit <saniket919@gmail.com>
2022-09-28 01:07:12 +0530

    good night commit

--- lib/recognizer_screen.dart
+++ lib/recognizer_screen.dart
@@ -17,7 +17,38 @@ class _RecognizerScreen extends State<RecognizerScreen> {
         title: Text(widget.title),
       ),
       body: Container(
-        child: Text('Hi there'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(16),
+                color: Colors.red,
+                alignment: Alignment.center,
+                child: Text('Header'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.green,
+              child: SizedBox(
+                width: 200,
+                height: 300,
+                child: Text('Canvas'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(16),
+                color: Colors.blue,
+                alignment: Alignment.center,
+                child: Text('Footer'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
